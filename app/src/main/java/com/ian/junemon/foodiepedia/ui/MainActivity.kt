83f3be6762277b3c059b7f18d408bb5f515be042
ian damping <iandamping92@gmail.com>
2020-01-28 11:19:16 +0700

    splash activity done with no problem

--- app/src/main/java/com/ian/junemon/foodiepedia/ui/MainActivity.kt
+++ app/src/main/java/com/ian/junemon/foodiepedia/ui/MainActivity.kt
@@ -2,8 +2,8 @@ package com.ian.junemon.foodiepedia.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.ian.junemon.foodiepedia.R
+import com.ian.junemon.foodiepedia.activityComponent
+import com.ian.junemon.foodiepedia.databinding.ActivityMainBinding
 
 /**
  *
@@ -11,9 +11,15 @@ Created by Ian Damping on 07/06/2019.
 Github = https://github.com/iandamping
  */
 class MainActivity : AppCompatActivity() {
-
+    val mainActivityComponent by lazy { activityComponent().inject(this) }
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.run {
+            lifecycleOwner = this@MainActivity
+        }
+        val view = binding.root
+        setContentView(view)
     }
 }
